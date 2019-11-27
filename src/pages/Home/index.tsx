63cd5ff5b9f676391ff6a26cea9597827e44a291
zhenglipeng <zhenglipeng@sensetime.com>
2019-11-27 15:28:23 +0800

    首页修改

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Link from 'umi/link';
 
+import style from './style.less';
+
 export default function(props: any) {
 
   return (
     <div>
-      <h2>Puzzle</h2>
-      
-      <Link to="/editor">编辑器</Link>
-      <Link to="/test">测试</Link>
+      <header className={style.header}>
+        <h1>Puzzle</h1>
+        <h2>Puzzle is a simple but powerful tool for designers and developers.</h2>
+        
+        <Link to="/editor" className={style.btn}>开始编辑</Link>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="https://github.com/lanyuechen/puzzle" className={style.btn}>Github</a>
+      </header>
     </div>
   );
 }
