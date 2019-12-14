15987a65e5faf90f4a171ec59a668120eb9e8d0e
zhenglipeng <1252012805@qq.com>
2019-12-14 18:17:02 +0800

    添加了header和主题切换

--- src/pages/Workspace/index.tsx
+++ src/pages/Workspace/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, createContext } from 'react';
+import React, { useEffect, createContext, useState } from 'react';
 import { connect } from 'dva';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
@@ -20,6 +20,7 @@ export const WorkspaceContext = createContext({
 const Workspace = (props: any) => {
   const { dispatch, workspace } = props;
   const { component, projects, actives, current } = workspace;
+  const [ theme, setTheme ] = useState(localStorage.theme || 'dark');
 
   useEffect(() => {
     dispatch({
@@ -33,6 +34,11 @@ const Workspace = (props: any) => {
       type: 'workspace/save',
     });
   }, [workspace]);
+
+  const handleTheme = () => {
+    localStorage.theme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(localStorage.theme);
+  };
 
   const handleTabsChange = (key: string, act: string) => {
     if (act === 'remove') {
@@ -59,42 +65,55 @@ const Workspace = (props: any) => {
   return (
     <DndProvider backend={HTML5Backend}>
       <WorkspaceContext.Provider value={{libs: antd}}>
-        <Layout>
-          <Layout.Sider className={style.sider} theme="light" width={256}>
-            <Tabs tabPosition="left" className={style.tabsLeft}>
-              <Tabs.TabPane key="files" tab={<Icon type="folder" />}>
-                <Project projects={projects} current={current} dispatch={dispatch} />
-              </Tabs.TabPane>
-              <Tabs.TabPane key="antd" tab={<Icon type="appstore" />}>
-                <Elements />
-              </Tabs.TabPane>
-              <Tabs.TabPane key="other" tab={<Icon type="smile" />}>
-                there is nothing
-              </Tabs.TabPane>
-            </Tabs>
-          </Layout.Sider>
-          <Layout.Content className={style.content}>
-            <Tabs
-              className={style.tabsRight}
-              type="editable-card"
-              hideAdd
-              activeKey={current}
-              onEdit={(key: any, action: any) => handleTabsChange(key, action)}
-              onChange={(key: string) => handleTabsChange(key, 'select')}
-            >
-              {actives && actives.map((path: any) => (
-                <Tabs.TabPane
-                  key={path} 
-                  tab={_.get(projects, path, {}).name}
-                >
-                  <Editor
-                    data={component[path]}
-                    onChange={(data: any) => handleEdit(path, data)}
-                  />
+        <Layout
+          style={theme === 'dark' ? {filter: 'invert(1)', overflow: 'hidden'} : {}}
+          // filter存在时会出现滚动条，原因未知，所以添加overflow: hidden去掉滚动条
+        >
+          <Layout.Header className={style.header}>
+            Puzzle
+            <Icon 
+              type={theme === 'dark' ? 'frown' : 'smile'}
+              onClick={handleTheme}
+              style={{float: 'right', marginTop: 5, cursor: 'pointer'}}
+            />
+          </Layout.Header>
+          <Layout className={style.body}>
+            <Layout.Sider className={style.sider} theme="light" width={256}>
+              <Tabs tabPosition="left" className={style.tabsLeft}>
+                <Tabs.TabPane key="files" tab={<Icon type="folder" />}>
+                  <Project projects={projects} current={current} dispatch={dispatch} />
                 </Tabs.TabPane>
-              ))}
-            </Tabs>
-          </Layout.Content>
+                <Tabs.TabPane key="antd" tab={<Icon type="appstore" />}>
+                  <Elements />
+                </Tabs.TabPane>
+                <Tabs.TabPane key="other" tab={<Icon type="smile" />}>
+                  there is nothing
+                </Tabs.TabPane>
+              </Tabs>
+            </Layout.Sider>
+            <Layout.Content className={style.content}>
+              <Tabs
+                className={style.tabsRight}
+                type="editable-card"
+                hideAdd
+                activeKey={current}
+                onEdit={(key: any, action: any) => handleTabsChange(key, action)}
+                onChange={(key: string) => handleTabsChange(key, 'select')}
+              >
+                {actives && actives.map((path: any) => (
+                  <Tabs.TabPane
+                    key={path} 
+                    tab={_.get(projects, path, {}).name}
+                  >
+                    <Editor
+                      data={component[path]}
+                      onChange={(data: any) => handleEdit(path, data)}
+                    />
+                  </Tabs.TabPane>
+                ))}
+              </Tabs>
+            </Layout.Content>
+          </Layout>
         </Layout>
       </WorkspaceContext.Provider>
     </DndProvider>
