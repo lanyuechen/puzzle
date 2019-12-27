e52dbd1ac89398b2444b8f108bd1fc87e68970f4
zhenglipeng <zhenglipeng@sensetime.com>
2019-12-27 17:28:44 +0800

    小修改

--- src/pages/Test/Workflow/Form/List.tsx
+++ src/pages/Test/Workflow/Form/List.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button } from 'antd';
+import { Form, Button, Row, Col } from 'antd';
 import _ from 'lodash';
 
 import evt from '../utils/event';
@@ -25,6 +25,10 @@ export default (props: any) => {
     onChange(config.path, [...value, undefined]);
   }
 
+  const handleRemove = (idx: number) => {
+    onChange(config.path, value.filter((v:any, i: number) => i !== idx));
+  }
+
   return (
     <Form.Item
       label={config.label || config.path}
@@ -34,14 +38,21 @@ export default (props: any) => {
       <div style={{border: '1px solid #ddd', padding: 8}}>
         {value.map((d: any, i: number) => (
           <div key={i}>
-            {React.Children.map(children, (child: any) => ({
-              ...child,
-              props: {
-                ...child.props,
-                data: d,
-                onChange: (path: string, value: any) => handleChange(i, path, value),
-              }
-            }))}
+            <a onClick={() => handleRemove(i)} style={{float: 'right', marginLeft: 8}}>删除</a>
+            <Row gutter={8} style={{overflow: 'hidden'}}>
+              {React.Children.map(children, (child: any) => (
+                <Col span={Math.floor(24 / React.Children.count(children))}>
+                  {{
+                    ...child,
+                    props: {
+                      ...child.props,
+                      data: d,
+                      onChange: (path: string, value: any) => handleChange(i, path, value),
+                    }
+                  }}
+                </Col>
+              ))}
+            </Row>
           </div>
         ))}
         <div>
